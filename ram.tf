cd1df809c8a52d9773280e4e26144be0c5ad0c48
Cris Daniluk <cdaniluk@rhythmictech.com>
2019-09-21 15:36:27 -0400

    Don't try to create resource shares when not explicitly needed to avoid errors on accounts that don't have RAM enabled

--- ram.tf
+++ ram.tf
@@ -8,11 +8,11 @@ resource "aws_ram_resource_share" "endpoint_share" {
 resource "aws_ram_principal_association" "endpoint_ram_principal" {
   count              = length(var.resource_share_accounts)
   principal          = var.resource_share_accounts[count.index]
-  resource_share_arn = aws_ram_resource_share.endpoint_share.arn
+  resource_share_arn = aws_ram_resource_share.endpoint_share[0].arn
 }
 
 resource "aws_ram_resource_association" "endpoint_ram_resource" {
   count			    = length(var.resource_share_accounts) > 0 ? 1 : 0
   resource_arn       = aws_route53_resolver_rule.fwd.arn
-  resource_share_arn = aws_ram_resource_share.endpoint_share.arn
+  resource_share_arn = aws_ram_resource_share.endpoint_share[0].arn
 }
